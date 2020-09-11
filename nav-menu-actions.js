5935900c637302b7fe444c58ff063349cc29f974
vesterlis <vesterlis@ya.ru>
2020-09-11 21:11:26 +0300

    Layout #7
    Оживила слайдер в блоке .advantages

--- nav-menu-actions.js
+++ nav-menu-actions.js
@@ -32,6 +32,23 @@ yourAccount__crossButton.onclick = function () {
 }
 
 
+let slide1 = document.getElementById('slide1');
+let slide2 = document.getElementById('slide2');
+let slide3 = document.getElementById('slide3');
+let caret = document.querySelector('.advantages__list')
+
+
+slide1.addEventListener('click', function () {
+    caret.style.transform = ' translateX(0%)'
+})
+
+slide2.addEventListener('click', function () {
+    caret.style.transform = ' translateX(-100%)'
+})
+
+slide3.addEventListener('click', function () {
+    caret.style.transform = ' translateX(-200%)'
+})
 
 
 
